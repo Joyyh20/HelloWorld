1114a8b5e90c5879bf8f29dd32c36e35d5fbd30c
Joyce <joyyh20@gmail.com>
2024-10-15 11:49:36 -0300

    Create app01.py

---
+++ app01.py
@@ -0,0 +1,35 @@
+# Cria uma variável
+
+'''
+esse comentário pode
+ter muitas
+e muitas 
+linhas
+
+'''
+# Cria uma variável usando "snake_case" 
+cor_da_casa = 'azul'
+
+# Exibe as coisas
+print(cor_da_casa)
+ 
+
+# Criar uma variável usando "camelCase"
+cordaCasa = "amarela"
+ 
+# Exibe coisas
+print('\n\n\n')
+print(cor_da_casa)
+print(cordaCasa)
+
+# Primeiro nome
+name_first = "Joyce"
+# Último nome
+name_last = "Pereira"
+
+# Imprime nome
+print(name_first)
+print(name_last)
+
+#imprime os dois nomes
+print(name_first,name_last)
